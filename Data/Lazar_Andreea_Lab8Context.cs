43d7b7e0b27b0eeebe9168a7aa02bf4628e26861
ANDREEA-MARIA LAZAR <andreea.maria.lazar@stud.ubbcluj.ro>
2020-11-29 19:50:38 +0200

    Add project files.

---
+++ Data/Lazar_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lazar_Andreea_Lab8.Models;
+
+namespace Lazar_Andreea_Lab8.Data
+{
+    public class Lazar_Andreea_Lab8Context : DbContext
+    {
+        public Lazar_Andreea_Lab8Context (DbContextOptions<Lazar_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lazar_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Lazar_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Lazar_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lazar_Andreea_Lab8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Lazar_Andreea_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Lazar_Andreea_Lab8.Data.Lazar_Andreea_Lab8Context _context;
+
+        public IndexModel(Lazar_Andreea_Lab8.Data.Lazar_Andreea_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
